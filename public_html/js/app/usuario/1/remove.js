0bf663e790c77eceb18d3936f7366e43941b7c45
rafa aznar <rafaaznar@gmail.com>
2017-11-27 22:26:37 +0100

    remove

--- public_html/js/app/usuario/1/remove.js
+++ public_html/js/app/usuario/1/remove.js
@@ -29,8 +29,8 @@
 'use strict';
 
 moduloUsuario.controller('UsuarioRemove1Controller',
-        ['$scope', '$routeParams', 'serverCallService', '$location', 'sessionService', 'constantService','objectService',
-            function ($scope, $routeParams, serverCallService, $location, sessionService, constantService,objectService) {
+        ['$scope', '$routeParams', 'serverCallService', '$location', 'sessionService', 'constantService', 'objectService',
+            function ($scope, $routeParams, serverCallService, $location, sessionService, constantService, objectService) {
                 $scope.ob = "usuario";
                 $scope.op = "remove";
                 $scope.profile = 1;
@@ -47,7 +47,9 @@ moduloUsuario.controller('UsuarioRemove1Controller',
                     if (response.status == 200) {
                         if (response.data.status == 200) {
                             $scope.status = null;
-                            $scope.bean = response.data.json;
+                            $scope.bean = response.data.json.data;
+                            $scope.metao = response.data.json.metaObject;
+                            $scope.metap = response.data.json.metaProperties;
                         } else {
                             $scope.status = "Error en la recepción de datos del servidor";
                         }
