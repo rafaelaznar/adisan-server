d648d13d6680aa01cf1acd2346f145c5dd07e21e
a022583952e <a022583952e@S09-PC14>
2017-12-20 09:00:56 +0100

    Cliente actualizado con paciente

--- public_html/js/system/config/app.js
+++ public_html/js/system/config/app.js
@@ -46,7 +46,6 @@ var trolleyes = angular.module('myApp', [
     'linea_pedidoControllers',
     'sexoControllers',
     'circunstanciasaltaControllers',
-    'pacienteControllers',
     'episodioControllers',
     'tipopagoControllers',
     'tiposervicioControllers',
@@ -85,7 +84,6 @@ var moduloCircunstanciasalta = angular.module('circunstanciasaltaControllers', [
 var moduloEpisodio = angular.module('episodioControllers', []);
 var moduloTipopago = angular.module('tipopagoControllers', []);
 var moduloEspecialidad = angular.module('especialidadControllers', []);
-var moduloPaciente = angular.module('pacienteControllers', []);
 
 var moduloFactura = angular.module('facturaControllers', []);
 
