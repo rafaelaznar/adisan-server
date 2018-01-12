04232ba248b621f7be662a0e9fce889d0e420ea9
a022583952e <a022583952e@S09-PC14>
2018-01-12 12:57:26 +0100

    Averiguado el fallo orden

--- src/main/java/eu/rafaelaznar/dao/specificimplementation/MedicoProfesorSpecificDaoImplementation.java
+++ src/main/java/eu/rafaelaznar/dao/specificimplementation/MedicoProfesorSpecificDaoImplementation.java
@@ -33,10 +33,12 @@
 package eu.rafaelaznar.dao.specificimplementation;
 
 import eu.rafaelaznar.bean.genericimplementation.TableGenericBeanImplementation;
+import eu.rafaelaznar.bean.genericimplementation.ViewGenericBeanImplementation;
 import eu.rafaelaznar.bean.helper.FilterBeanHelper;
 import eu.rafaelaznar.bean.helper.MetaBeanHelper;
 import eu.rafaelaznar.bean.meta.helper.MetaObjectGenericBeanHelper;
 import eu.rafaelaznar.bean.meta.helper.MetaPropertyGenericBeanHelper;
+import eu.rafaelaznar.bean.publicinterface.GenericBeanInterface;
 import eu.rafaelaznar.bean.specificimplementation.UsuarioSpecificBeanImplementation;
 import eu.rafaelaznar.dao.genericimplementation.TableGenericDaoImplementation;
 import eu.rafaelaznar.factory.BeanFactory;
@@ -48,6 +50,8 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 /**
  *
@@ -136,7 +140,7 @@ public class MedicoProfesorSpecificDaoImplementation extends TableGenericDaoImpl
             oPreparedStatement = oConnection.prepareStatement(strSQL);
             oPreparedStatement.setInt(1, id);
             oResultSet = oPreparedStatement.executeQuery();
-            oBean = (TableGenericBeanImplementation) BeanFactory.getBean(ob,oPuserSecurity);;
+            oBean = (TableGenericBeanImplementation) BeanFactory.getBean(ob, oPuserSecurity);;
             if (oResultSet.next()) {
                 oBean = (TableGenericBeanImplementation) oBean.fill(oResultSet, oConnection, oPuserSecurity, intExpand);
             } else {
@@ -161,7 +165,6 @@ public class MedicoProfesorSpecificDaoImplementation extends TableGenericDaoImpl
         return oMetaBeanHelper;
     }
 
-    
     @Override
     public Long getCount(ArrayList<FilterBeanHelper> alFilter) throws Exception {
         strSQL = "SELECT count(*) FROM medico m, centrosanitario cs WHERE m.id_centrosanitario = cs.id AND cs.id = " + idCentrosanitario;
@@ -193,8 +196,60 @@ public class MedicoProfesorSpecificDaoImplementation extends TableGenericDaoImpl
         }
         return iResult;
     }
+
+    @Override
+    public MetaBeanHelper getPage(int intRegsPerPag, int intPage, LinkedHashMap<String, String> hmOrder, ArrayList<FilterBeanHelper> alFilter, int expand) throws Exception {
+        String strSQL1 = strSQL;
+        strSQL1 += SqlHelper.buildSqlFilter(alFilter);
+        strSQL1 += SqlHelper.buildSqlOrder(hmOrder);
+        strSQL1 += SqlHelper.buildSqlLimit(this.getCount(alFilter), intRegsPerPag, intPage);
+        ArrayList<ViewGenericBeanImplementation> aloBean = new ArrayList<>();
+        PreparedStatement oPreparedStatement = null;
+        ResultSet oResultSet = null;
+        MetaBeanHelper oMetaBeanHelper = null;
+        try {
+            oPreparedStatement = oConnection.prepareStatement(strSQL1);
+            oResultSet = oPreparedStatement.executeQuery(strSQL1);
+            while (oResultSet.next()) {
+                GenericBeanInterface oBean = BeanFactory.getBean(ob, oPuserSecurity);
+                oBean = (ViewGenericBeanImplementation) oBean.fill(oResultSet, oConnection, oPuserSecurity, expand);
+                aloBean.add((ViewGenericBeanImplementation) oBean);
+            }
+
+            ArrayList<MetaPropertyGenericBeanHelper> alMetaProperties = this.getPropertiesMetaData();
+            MetaObjectGenericBeanHelper oMetaObject = this.getObjectMetaData();
+            oMetaBeanHelper = new MetaBeanHelper(oMetaObject, alMetaProperties, aloBean);
+
+        } catch (Exception ex) {
+            String msg = this.getClass().getName() + ":" + (ex.getStackTrace()[0]).getMethodName() + " ob:" + ob;
+            Log4jHelper.errorLog(msg, ex);
+            throw new Exception(msg, ex);
+        } finally {
+            if (oResultSet != null) {
+                oResultSet.close();
+            }
+            if (oPreparedStatement != null) {
+                oPreparedStatement.close();
+            }
+        }
+        return oMetaBeanHelper;
+    }
     
-    
+     //Para arreglar el ordenar 12-1-18
+     public String buildSqlOrder(LinkedHashMap<String, String> hmOrder) {
+        String strSQLOrder = "";
+        if (hmOrder != null) {
+            for (Map.Entry<String, String> entry : hmOrder.entrySet()) {
+                strSQLOrder += "m."+entry.getKey();                
+                strSQLOrder += " ";
+                strSQLOrder += entry.getValue();
+                strSQLOrder += ",";
+            }
+            strSQLOrder = " ORDER BY " + strSQLOrder.substring(0, strSQLOrder.length() - 1);
+        }
+        return strSQLOrder;
+    }
+
     public Boolean checkUpdate(int id) {
         if (id != 0) {
             return true;
@@ -202,9 +257,7 @@ public class MedicoProfesorSpecificDaoImplementation extends TableGenericDaoImpl
             return false;
         }
     }
+    
+    
 
-    
-    
-    
-    
 }
