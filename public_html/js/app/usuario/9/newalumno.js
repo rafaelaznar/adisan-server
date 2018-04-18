93d25d479c4240538a5e40c3b76a71b0633d2f03
raznara <raznara@s09-prof>
2018-04-18 13:12:24 +0200

    limpieza

--- public_html/js/app/usuario/9/newalumno.js
+++ public_html/js/app/usuario/9/newalumno.js
@@ -5,22 +5,14 @@ moduloUsuario.controller('UsuarioNewalumno9Controller',
                 $scope.ob = "usuario";
                 $scope.op = "new";
                 $scope.profile = 1;
-
                 $scope.status = null;
                 $scope.debugging = constantService.debugging();
                 $scope.url = $scope.ob + '/' + $scope.profile + '/' + $scope.op;
-
                 $scope.title = "Registro de alumno para el grupo " + $routeParams.codigo;
                 $scope.profile = 9;
-
-
-
-
                 $scope.bean = {};
                 $scope.codigoGrupo = $routeParams.codigo;
                 $scope.fase = 1;
-
-
                 //obtener información del profesor y del curso y del centro a partir del código de centro
                 $http.get(constantService.getAppUrl() + '?ob=usuario&op=getidcurso&codigo=' + $routeParams.codigo, 'GET', '').then(function (response) {
                     if (response.status == 200) {
@@ -37,11 +29,6 @@ moduloUsuario.controller('UsuarioNewalumno9Controller',
                     //$scope.outerForm.login.$setValidity('repetido', true);
                     return false;
                 });
-
-                $scope.back = function () {
-                    window.history.back();
-                };
-
                 $scope.validausuario = function (field) {
                     if ($scope.fase == 1) {
                         if ($scope.bean.login) {
@@ -61,44 +48,18 @@ moduloUsuario.controller('UsuarioNewalumno9Controller',
                             });
                         }
                     }
-                }
+                };
                 $scope.checkPass = function () {
                     if ($scope.bean.password == $scope.bean.password2) {
                         return false;
                     } else {
                         return true;
                     }
-                }
+                };
                 $scope.validaContrasenya = function () {
                     $scope.fase = 3;
-                }
-//        $scope.anyInvalid = function (form) {
-//            for (var prop in form) {
-//                if (form.hasOwnProperty(prop)) {
-//                    if (form[prop].$invalid) {
-//                        return true;
-//                    }
-//                }
-//            }
-//            return false;
-//        };
-//        $scope.anyDirtyAndInvalid = function (form) {
-//            for (var prop in form) {
-//                if (form.hasOwnProperty(prop)) {
-//                    if (form[prop].$dirty && form[prop].$invalid) {
-//                        return true;
-//                    }
-//                }
-//            }
-//            return false;
-//        };
-
+                };
                 $scope.send = function () {
-//            $scope.bean.creation = $filter('date')($scope.bean.creation, "dd/MM/yyyy");
-//            $scope.bean.modification = $filter('date')($scope.bean.modification, "dd/MM/yyyy");
-//            if (!$scope.bean.obj_medico.id > 0) {
-//                $scope.bean.obj_medico.id = null;
-//            }
                     delete $scope.bean.password2;
                     $scope.bean.id_grupo = $scope.grupo.data.id;
                     $scope.bean.id_centro = $scope.grupo.data.obj_usuario.data.obj_centro.data.id;
@@ -124,6 +85,9 @@ moduloUsuario.controller('UsuarioNewalumno9Controller',
                         $scope.status = "Error en la recepción de datos del servidor. Indica el error al administrador del sistema.";
                     });
                     ;
+                };
+                $scope.back = function () {
+                    window.history.back();
                 };
             }
         ]);
