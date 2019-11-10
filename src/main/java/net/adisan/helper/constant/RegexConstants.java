2c19d1d8443aeeb9073d0b1935830d1103fc141c
rafael aznar <rafaaznar@gmail.com>
2019-11-10 19:04:25 +0100

    mejora de expresiones y caracteres en regex

--- src/main/java/net/adisan/helper/constant/RegexConstants.java
+++ src/main/java/net/adisan/helper/constant/RegexConstants.java
@@ -37,10 +37,10 @@ public class RegexConstants {
     public static final String dni = "[0-9]{8,8}[A-Z]";
     public static final String dni_Help = "ocho dígitos y una letra mayúscula";
     public static final String code = "[A-Z]+[a-z0-9.\\-_çñáéíóúàèò]+";
-    public static final String code_Help = "Mayúscula y letras o dígitos separados por guión";
+    public static final String code_Help = "mayúscula y letras o dígitos separados por guión";
     public static final String capscode = "[A-Z0-9_-]*";
-    public static final String capscode_Help = "Mayúsculas o dígitos separados por guión";      
-    public static final String capitalizedName = "([A-Z]{1}[a-zçñáéíóúàèò,\\- ]+[\\s]*)+";
+    public static final String capscode_Help = "mayúsculas o dígitos separados por guión";      
+    public static final String capitalizedName = "([A-Z]{1}[a-zçñáéíóúàèòü,\\-\\ ]+[\\s]*)+";
     public static final String capitalizedName_Help = "una o varias palabras que todas comienzan en mayúscula";
     public static final String nameWithEndingNumbers = "[a-z][a-z0-9 ]+";
     public static final String nameWithEndingNumbers_Help = "un texto en minúsculas que puede acabar en dígitos";
@@ -48,6 +48,6 @@ public class RegexConstants {
     public static final String email_Help = "un email válido"; // see \\ is \    
     public static final String capitalizedSentence = "[A-Z]([^\\s]*\\s?)+";
     public static final String capitalizedSentence_Help = "una frase que comienza en mayúscula";
-    public static final String direction = "[A-Z]{1}[a-z0-9ºçñáéíóúàèò- ]*";
+    public static final String direction = "[A-Z]{1}[a-z0-9ºçñáéíóúàèòü- ]*";
     public static final String directionSentence_Help = "una direccion que comienza en mayúscula";
 }
